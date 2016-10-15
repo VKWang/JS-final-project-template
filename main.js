f9f017026777cf42cb30aa07554d410ec23d24ee
VKWang <VKWang914@gmail.com>
2016-10-15 16:53:48 +0800

    starts

---
+++ main.js
@@ -0,0 +1,13 @@
+var bglmg = ducument.createElement("img");
+
+bglmg.src = "images/map.png";
+
+var canvas = document.getElementByid("game-canvas");
+
+var ctx = canvas.getContext("2d");
+
+function draw(){
+  ctx.drawlmage(bglmg,0,0);
+}
+
+draw();
