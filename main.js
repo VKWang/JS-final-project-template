07d5115f092150ac1a788c88c3e0b435a6e147dc
VKWang <VKWang914@gmail.com>
2016-10-15 17:32:10 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 var bglmg = document.createElement("img");
 bglmg.src = "images/map.png";
+
+var enemyImg = document.createElement("img");
+enemyImg.src = "images/slime.gif";
+
 var canvas = document.getElementById("game-canvas");
 var ctx = canvas.getContext("2d");
 function draw(){
