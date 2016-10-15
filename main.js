9121300698bb20aba42498eab98923d74c6d90c8
VKWang <VKWang914@gmail.com>
2016-10-15 17:03:36 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-var bglmg = ducument.createElement("img");
-bglmg.src = "images/map.png";
+var bglmg = ducument.createElement("slime.gif");
+bglmg.src = "slime.gif";
 var canvas = document.getElementByid("game-canvas");
 var ctx = canvas.getContext("2d");
 function draw(){
