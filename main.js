c7118bf68cf0669baeabaf146edc471ad771ca6c
VKWang <VKWang914@gmail.com>
2016-10-15 16:59:08 +0800

    settimeout

--- main.js
+++ main.js
@@ -1,13 +1,8 @@
 var bglmg = ducument.createElement("img");
-
 bglmg.src = "images/map.png";
-
 var canvas = document.getElementByid("game-canvas");
-
 var ctx = canvas.getContext("2d");
-
 function draw(){
   ctx.drawlmage(bglmg,0,0);
 }
-
-draw();
+seTimeout(draw , 125);
