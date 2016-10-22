21d88c56e7d27c021365564934f699d7e872c246
VKWang <VKWang914@gmail.com>
2016-10-22 18:14:48 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
-var bglmg = document.createElement("img");
+var bgImg = document.createElement("img");
 bglmg.src = "images/map.png";
-
-var btnImg = document.createElement("img");
-btnImg.src = "images/tower-btn.png";
 
 var enemy ={x:430,y:80};
 var enemyImg = document.createElement("img");
 enemyImg.src = "images/slime.gif";
+
+var btnImg = document.createElement("img");
+btnImg.src = "images/tower-btn.png";
+
+function isCollided(pointX,pointY,targetX,targetY,targetWidth,targetLenth){
+  if(   pointX >= targetX
+     && pointX <= targetX+targetWidth
+     && pointY >= targetY
+     && pointY <= targetY+targetLenth){
+    return true
+  }else{
+    return false
+}
 
 var cursor = {x:0 , y:0};
 $("#game-canvas").mousemove(function(event){
@@ -21,9 +31,23 @@ towerImg.src = "images/tower.png";
 var canvas = document.getElementById("game-canvas");
 var ctx = canvas.getContext("2d");
 function draw(){
-  ctx.drawImage(bglmg,0,0);
+  ctx.drawImage(bgImg,0,0);
   ctx.drawImage(enemyImg,enemy.x,enemy.y);
   ctx.drawImage(btnImg,578,418,62,62);
   ctx.drawImage(towerImg,cursor.x,cursor.y);
 }
+
 setInterval(draw,1000/60);
+  
+var isBuilding = false;
+var tower = {};
+var cursor = {};
+
+$("#game-canvas").on(click , function(){
+  if(isCollided(curser.x,curser.y.,640-62,480-62,62,62)){
+    if(isBuilding=false){
+      isBuilding = true;
+    }else{
+      isBuilding = false;
+    }
+    ;})
