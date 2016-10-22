62d2ed83a663b5da823ad5328fc67f24b3feea63
VKWang <VKWang914@gmail.com>
2016-10-22 16:44:05 +0800

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ bglmg.src = "images/map.png";
 var btnImg = document.createElement("img");
 btnImg.scr = "images/tower.png";
 
-var enemy ={x:400,y:400};
+var enemy ={x:640,y:480};
 var enemyImg = document.createElement("img");
 enemyImg.src = "images/slime.gif";
 
