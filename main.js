a39cba39d23e5a7f16cdaeef932d9fff04c8f777
VKWang <VKWang914@gmail.com>
2016-10-22 16:52:23 +0800

    Update main.js

--- main.js
+++ main.js
@@ -13,6 +13,6 @@ var ctx = canvas.getContext("2d");
 function draw(){
   ctx.drawImage(bglmg,0,0);
   ctx.drawImage(enemyImg,enemy.x,enemy.y);
-  ctx.drawImage(btnImg,600,440,60,60);
+  ctx.drawImage(btnImg,520,400,60,60);
 }
 setInterval(draw,1000/60);
