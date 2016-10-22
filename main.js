6506fb592f6f4b346ab2d163d72bf6ee261f0ca4
VKWang <VKWang914@gmail.com>
2016-10-22 16:10:51 +0800

    Update main.js

--- main.js
+++ main.js
@@ -2,8 +2,6 @@ var bglmg = document.createElement("img");
 bglmg.src = "images/map.png";
 
 var enemy ={x:0,y:0};
-var enemy = document.createElement("img");
-enemy.scr = "images/slime.gif";
 
 var enemyImg = document.createElement("img");
 enemyImg.src = "images/slime.gif";
@@ -12,7 +10,6 @@ var canvas = document.getElementById("game-canvas");
 var ctx = canvas.getContext("2d");
 function draw(){
   ctx.drawImage(bglmg,0,0);
-  ctx.drawImage(enemyImg,0,300);
-  ctx.drawImage(enemy,enemy.x,enemy.y);
+  ctx.drawImage(enemyImg,enemy.x,enemy.y);
 }
 setTimeout(draw , 125);
