e7ebf49f3be2dab159b4a90289ee8cfb30cc3301
VKWang <VKWang914@gmail.com>
2016-10-22 16:50:55 +0800

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ bglmg.src = "images/map.png";
 var btnImg = document.createElement("img");
 btnImg.src = "images/tower-btn.png";
 
-var enemy ={x:400,y:40};
+var enemy ={x:430,y:80};
 var enemyImg = document.createElement("img");
 enemyImg.src = "images/slime.gif";
 
@@ -13,6 +13,6 @@ var ctx = canvas.getContext("2d");
 function draw(){
   ctx.drawImage(bglmg,0,0);
   ctx.drawImage(enemyImg,enemy.x,enemy.y);
-  ctx.drawImage(btnImg,600,440,10,10);
+  ctx.drawImage(btnImg,600,440,60,60);
 }
 setInterval(draw,1000/60);
