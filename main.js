ea699260ab19d67888640b1963614c2498b1b8f6
VKWang <VKWang914@gmail.com>
2016-10-29 17:49:34 +0800

    Update main.js

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ function draw(){
 }
 setInterval(draw,1000/60);
 
-function build(isBuilding !== false){
+function build(isBuilding() !== false){
   var towerImg = document.createElement("img");
   towerImg.src = "images/tower.png";
 }
