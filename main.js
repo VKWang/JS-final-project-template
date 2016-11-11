4fef23bf11bf48647b0a77b980141da82769ec91
VKWang <VKWang914@gmail.com>
2016-11-11 23:05:24 +0800

    Update main.js

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ var enemyPath = [
   {x:384,y:64,speedx:64,speedy:0},
   {x:384,y:192,speedx:0,speedy:64},
   {x:224,y:192,speedx:-64,speedy:0},
+  {x:224,y:320,speedx:0,speedy:64},
   {x:0,y:0,speedx:0,speedy:0}
 ];
 var enemy = {
