523bc1deb680db185dd4f600f433322b9c3f3ea5
VKWang <VKWang914@gmail.com>
2016-11-12 21:04:00 +0800

    Update main.js

--- main.js
+++ main.js
@@ -97,7 +97,7 @@ var ctx = canvas.getContext("2d");
 function draw(){
   ctx.drawImage(bgImg,0,0);
   ctx.drawImage(btnImg,btn.x,btn.y,btn.width,btn.height);
-  ctx.fillText("1000/1000HP",50,10);
+  ctx.fillText("1000/1000HP",10,32);
   ctx.font = "22px Arial";
   ctx.fillStyle = "white";
   if((clock % 100) == 0){
