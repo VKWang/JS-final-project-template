d7ea437e379fdfb257af484ad8ac66aab483532b
VKWang <VKWang914@gmail.com>
2016-11-19 18:08:47 +0800

    Update main.js

--- main.js
+++ main.js
@@ -64,7 +64,7 @@ var tower = {
   searchEnemy: function(){
     for(var i=0;i<enemies.length;i++){
       var distance = Math.sprt(
-        Math.pow(this.x-enemies[i].x,2)+Math.pow(this.y-enemyies[i].y,2)
+        Math.pow(this.x-enemies[i].x,2)+Math.pow(this.y-enemies[i].y,2)
       );
       if(distance <= this.range){
         this.aimingEnemyId = i;
