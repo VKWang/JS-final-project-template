a3a2cf8b2149fbe4b6d092ac2fdbb510e9ec5437
VKWang <VKWang914@gmail.com>
2016-11-26 17:00:20 +0800

    Update main.js

--- main.js
+++ main.js
@@ -167,7 +167,7 @@ function draw(){
   }
   tower.searchEnemy();
   if(tower.aimingEnemyId != null){
-    //var id = tower.aimingEnemyId;
+    var id = tower.aimingEnemyId;
     ctx.drawImage(crosshairImg,enemies[id].x,enemies[id].y,32,32);
   }
   ctx.drawImage(towerImg,tower.x,tower.y,tower.width,tower.height);
