db8772105f3bbe80530f6a7195fe87c31227dd01
VKWang <VKWang914@gmail.com>
2016-12-06 21:13:35 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
+var bgImg = document.createElement("img");
+bgImg.src = "images/map.png";
 
+
+var FPS = 20;
+
+
+var canvas = document.getElementById("game-canvas");
+
+
+var ctx = canvas.getContext("2d");
+
+
+function draw(){
+  ctx.drawImage(bgImg,0,0);
+};
+
+
+setInterval(draw , 1000/FPS);
