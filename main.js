5d369ef28d76f62790309c83d049f3c547b5bfc6
VKWang <VKWang914@gmail.com>
2016-12-06 21:18:57 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 var bgImg = document.createElement("img");
 bgImg.src = "images/map.png";
+var btnImg = document.createElement("img");
+btnImg.src = "images/tower-btn.png"
 
 
 var FPS = 20;
+var btn = {
+  x:578,
+  y:418,
+  width:64,
+  height:64
+};
 
 
 var canvas = document.getElementById("game-canvas");
@@ -13,6 +21,7 @@ var ctx = canvas.getContext("2d");
 
 function draw(){
   ctx.drawImage(bgImg,0,0);
+  ctx.drawImage(btnImg,btn.x,btn.y,btn.width,btn.height);
 };
 
 
